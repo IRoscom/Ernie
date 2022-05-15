762464c2c51c9664f58263c5a0add36a10421c96
Максим <53787123+IRoscom@users.noreply.github.com>
2022-05-16 00:26:34 +1000

    Update interactionCreate.js

--- src/events/Interactions/interactionCreate.js
+++ src/events/Interactions/interactionCreate.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
+const { MessageEmbed, MessageButton, MessageActionRow, Guild } = require('discord.js');
 const { color, role, verified } = require('../../../config.json');
 const emoji = require('../../json/emoji.json');
 
@@ -6,24 +6,19 @@ module.exports = async (client, interaction) => {
     if (interaction.isButton()) {
         const embed = new MessageEmbed()
             .setColor(color)
-        if (!interaction.member._roles.includes(verified)) return
         if (interaction.customId == 'captcha') {
             const random = Math.floor(Math.random() * emoji.length);
             const row = new MessageActionRow()
                 .addComponents(buttons());
             embed.setDescription(`Для прохождения верификации нажмите на емодзи \`${emoji[random].name}\``)
             interaction.reply({ embeds: [embed], ephemeral: true, components: [row] });
-            filter = i => i.customId == emoji[random].value && i.user.id == interaction.user.id
-            collector = interaction.channel.createMessageComponentCollector({ filter, max: 1 });
+            collector = interaction.channel.createMessageComponentCollector({ max: 1 });
             collector.on('collect', async i => {
-                if (!i.guild.me.permissions.has('MANAGE_ROLES')) return i.update({ embeds: [embed.setDescription('У меня нет прав чтоб выдать вам роль. Обратитесь к администратору.')], components: [] })
-                try {
-                    if (role) await i.member.roles.add(role);
+                if (i.customId == emoji[random].value) {
+                    if (!i.guild.me.permissions.has('MANAGE_ROLES')) return i.update({ embeds: [embed.setDescription('У меня нет прав чтоб выдать вам роль. Обратитесь к администратору.')], components: [] })
+                    await i.member.roles.add(role);
                     await i.member.roles.remove(verified);
                     i.update({ embeds: [embed.setDescription('Верификация пройдена.')], components: [] })
-                } catch(err) {
-                    console.error(err)
-                    i.update({ embeds: [embed.setDescription('Кажется что-то пошло не так.').setFooter(`Ошибка: ${err.message}`)], components: []})
                 }
             })
         }
